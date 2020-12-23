b91936c644832eefbe06718a65d268aaf54d123e
Kaine <virtualdisaster782@gmail.com>
2020-12-23 00:42:34 +0000

    dylan pls help

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,43 @@
-import { Client } from 'discord.js'
-import { token } from "./config.json"
-import { t3roleID } from "./config.json"
-import { roles } from "./config.json"
-const bot = new Client()
-//Clasic imports
+import { Client } from 'discord.js';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 
-bot.on("ready", async () => {
-    if (bot.user === null) return
-    console.log(`${bot.user.tag} is online!`);
-    await bot.user.setActivity('your subs', { type: "WATCHING" })
+    let fileContents = fs.readFileSync('config.yml', 'utf8');
+    let data = yaml.safeLoad(fileContents);
+    
+
+    console.log(data);
+
+// const bot = new Client()
 
 
-})
-//checking when online
+// bot.on("ready", async () => {
+//     if (bot.user === null) return
+//     console.log(`${bot.user.tag} is online!`);
+//     await bot.user.setActivity('your subs', { type: "WATCHING" })
 
-bot.on("guildMemberUpdate", async (oldMember, newMember) => {
-    //Checking for role updates
+// })
 
-    if (oldMember.roles.cache.get(t3roleID) && !newMember.roles.cache.get(t3roleID)) {
-        //check if member update was removing the tier 3 role
-        const memberRoles = newMember.roles.cache
-        for (const role of roles) {
-            const invalidRole = memberRoles.get(role)
+// bot.on("guildMemberUpdate", async (oldMember, newMember) => {
 
-            if (invalidRole) {
-                await newMember.roles.remove(role, "No Longer Tier 3")
+//     if (oldMember.roles.cache.get(t3roleID) && !newMember.roles.cache.get(t3roleID)) {
+//         //check if member update was removing the tier 3 role
+//         const memberRoles = newMember.roles.cache
+//         //for loop checking and removing all colour roles listed in roles from config.json
+//         for (const role of roles) {
+//             const invalidRole = memberRoles.get(role)
 
-            }
+//             if (invalidRole) {
+//                 await newMember.roles.remove(role, "No Longer Tier 3")
 
-        }//for loop checking and removing all colour roles listed in roles from config.json
+//             }
 
-    }
+//         }
+
+//     }
 
 
-})
+// })
 
-bot.login(token)
+// bot.login()
